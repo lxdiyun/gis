b343943a8095d353331d8810e5eb79b40fb72656
Adrain Lee <lxdiyun@gmail.com>
2013-12-20 01:01:02 +0800

    ui changed

--- inscription/static/inscription/index_nav.js
+++ inscription/static/inscription/index_nav.js
@@ -6,11 +6,11 @@ $('#area_list li a').click( function(e){
 		id = $(this).attr('id');
 		Dajaxice.inscription.display_area(Dajax.process,{'area_id':id});
 
-		$("#location_info" ).fadeOut('fast');
-		$("#sub_location_info" ).fadeOut('fast');
+		$("#area_or_location_info" ).fadeOut('fast');
+		$("#sublocation_info" ).fadeOut('fast');
 		$("#inscription_info" ).fadeOut('fast');
-		$("#location_info" ).empty();
-		$("#sub_location_info" ).empty();
+		$("#area_or_location_info" ).empty();
+		$("#sublocation_info" ).empty();
 		$("#inscription_info" ).empty();
 		clean_all_markers();
 	}
