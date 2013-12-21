0f6ecd0f3716e5073d82d18b6b2b061f8075c7af
lxdiyun <lxdiyun@gmail.com>
2013-12-21 22:45:35 +0800

    add cover

--- inscription/admin.py
+++ inscription/admin.py
@@ -1,16 +1,26 @@
 from django.contrib.gis import admin
+from django.utils.translation import ugettext_lazy as _
+from imagekit.admin import AdminThumbnail
+
 from utils.admin import PointAdminBase, PhotoInlineBase
-from models import Area, Location, SubLocation, Inscription, Photo
+from models import (Area, Location, SubLocation, Inscription, Photo)
 
 
 class PhotoInline(PhotoInlineBase):
     model = Photo
 
 
+class AreaAdmin(admin.ModelAdmin):
+    readonly_fields = ['admin_thumbnail']
+    admin_thumbnail = AdminThumbnail(image_field='cover_thumbnail')
+
+    admin_thumbnail.short_description = _('Cover Thumbnail')
+
+
 class InscriptionAdmin(admin.ModelAdmin):
     inlines = [PhotoInline]
 
-admin.site.register(Area)
+admin.site.register(Area, AreaAdmin)
 admin.site.register(Location, PointAdminBase)
 admin.site.register(SubLocation)
 admin.site.register(Inscription, InscriptionAdmin)
