b0512df7e7bfc80c06f4fba140d0ebbe44efc2c3
Adrain Lee <lxdiyun@gmail.com>
2013-12-24 01:01:34 +0800

    adding fulltext search

--- inscription/search_indexes.py
+++ inscription/search_indexes.py
@@ -8,3 +8,24 @@ class AreaIndex(indexes.SearchIndex, indexes.Indexable):
 
     def get_model(self):
         return Area
+
+
+class LocationIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+
+    def get_model(self):
+        return Location
+
+
+class SubLocationIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+
+    def get_model(self):
+        return SubLocation
+
+
+class InscriptionIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+
+    def get_model(self):
+        return Inscription
